65844b4a252aa6e0c8dfbb745b29f89d87d2ca0c
Amrutha <156908852+Ammuzzamurutha@users.noreply.github.com>
2026-05-26 10:19:43 +0530

    Add files via upload

---
+++ madlibsgame.java
@@ -0,0 +1,14 @@
+public class madlibsgame {
+    public static void main(String[] args) {
+        String noun1 = "dog";
+        String noun2 = "cat";
+        String verb1 = "run";
+        String verb2 = "jump";
+        String adjective1 = "happy";
+        String adjective2 = "sad";
+
+        String story = "Once upon a time, there was a " + adjective1 + " " + noun1 + " who loved to " + verb1 + ". One day, it met a " + adjective2 + " " + noun2 + " who enjoyed to " + verb2 + ". They became friends and lived happily ever after.";
+        
+        System.out.println(story);
+    }
+}
